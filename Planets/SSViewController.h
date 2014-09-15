5a50b1db286bd1418d099e79f14f9a15be57abc9
Sid Sharma <sid@zineone.com>
2014-09-15 14:59:25 -0700

    Initial Commit

---
+++ Planets/SSViewController.h
@@ -0,0 +1,13 @@
+//
+//  SSViewController.h
+//  Planets
+//
+//  Created by Saurav Sharma on 9/15/14.
+//  Copyright (c) 2014 ZineOne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SSViewController : UIViewController
+
+@end
